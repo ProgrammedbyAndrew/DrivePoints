89fe6606e7406e53ce2c0403a1718e8d3b5339c4
Andrew <44824558+ProgrammedbyAndrew@users.noreply.github.com>
2023-12-22 20:54:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var modal = document.createElement('div');
-    modal.className = 'modal';
-    modal.innerHTML = `
-        <div class="modal-content">
-            <h2>Terms and Conditions</h2>
-            <p>Please read and accept our terms and conditions to use the Drive Rewards app.</p>
-            <button id="acceptTerms" class="btn">Accept</button>
-        </div>
-    `;
-    document.body.appendChild(modal);
-
     document.getElementById('acceptTerms').addEventListener('click', function() {
-        modal.style.display = 'none';
+        document.querySelector('.modal').style.display = 'none';
+        document.getElementById('mainPage').classList.remove('hidden');
     });
 });
