5369f875ecfd419931e413511b38333ba3c90da3
Andrew <44824558+ProgrammedbyAndrew@users.noreply.github.com>
2023-12-22 20:46:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Listen for click on the 'Accept Terms' button
     document.getElementById('acceptTerms').addEventListener('click', function() {
+        // Hide the terms page
         document.getElementById('termsPage').classList.add('hidden');
+        // Show the main page
         document.getElementById('mainPage').classList.remove('hidden');
     });
+
+    // Add any additional JavaScript for the main page here
 });
